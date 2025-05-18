d5aa2b9cf2087051806af9e158d9f40720597720
Lucas Nakahara <nakaharalucas2@gmail.com>
2025-05-17 23:26:17 -0300

    CommitUpdate

--- script.js
+++ script.js
@@ -1,55 +1,221 @@
-const tetris = document.getElementById("tetris");
-    const scoreDisplay = document.getElementById("score");
-    const width = 10;
-    let squares = [];
-    let score = 0;
+const canvas = document.getElementById('tetris');
+        const context = canvas.getContext('2d');
+        context.scale(20, 20); 
 
-    for (let i = 0; i < 200; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        tetris.appendChild(square);
-        squares.push(square);
-    }
+        const pieces = {
+            'I': [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ],
+            'J': [
+                [2, 0, 0],
+                [2, 2, 2],
+                [0, 0, 0],
+            ],
+            'L': [
+                [0, 0, 3],
+                [3, 3, 3],
+                [0, 0, 0],
+            ],
+            'O': [
+                [4, 4],
+                [4, 4],
+            ],
+            'S': [
+                [0, 5, 5],
+                [5, 5, 0],
+                [0, 0, 0],
+            ],
+            'T': [
+                [0, 6, 0],
+                [6, 6, 6],
+                [0, 0, 0],
+            ],
+            'Z': [
+                [7, 7, 0],
+                [0, 7, 7],
+                [0, 0, 0],
+            ]
+        };
 
-    const lTetromino = [
-      [1, width + 1, width * 2 + 1, 2],
-      [width, width + 1, width + 2, width * 2 + 2],
-      [1, width + 1, width * 2 + 1, width * 2],
-      [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ];
+        const colors = [
+            null,
+            'cyan',       
+            'blue',       
+            'orange',     
+            'yellow',     
+            'green',     
+            'purple',     
+            'red'         
+        ];
 
-    let currentPosition = 4;
-    let currentRotation = 0;
-    let current = lTetromino[currentRotation];
+        function getRandomPiece() {
+            const pieces_array = 'IJLOSTZ';
+            const randomPiece = pieces_array[Math.floor(Math.random() * pieces_array.length)];
+            return pieces[randomPiece];
+        }
 
-    function draw() {
-        current.forEach(index => {
-            squares[currentPosition + index].classList.add("tetromino");
+        function createMatrix(w, h) {
+            const matrix = [];
+            while (h--) {
+                matrix.push(new Array(w).fill(0));
+            }
+            return matrix;
+        }
+
+        function drawMatrix(matrix, offset) {
+            matrix.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        context.fillStyle = colors[value];
+                        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+                    }
+                });
+            });
+        }
+
+        function merge(arena, player) {
+            player.matrix.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        arena[y + player.pos.y][x + player.pos.x] = value;
+                    }
+                });
+            });
+        }
+
+        function collide(arena, player) {
+            const m = player.matrix;
+            const o = player.pos;
+            for (let y = 0; y < m.length; ++y) {
+                for (let x = 0; x < m[y].length; ++x) {
+                    if (m[y][x] !== 0 &&
+                        (arena[y + o.y] &&
+                            arena[y + o.y][x + o.x]) !== 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        function playerDrop() {
+            player.pos.y++;
+            if (collide(arena, player)) {
+                player.pos.y--;
+                merge(arena, player);
+                arenaSweep(); 
+                playerReset();
+            }
+            dropCounter = 0;
+        }
+
+        function playerMove(dir) {
+            player.pos.x += dir;
+            if (collide(arena, player)) {
+                player.pos.x -= dir;
+            }
+        }
+
+        function playerReset() {
+            player.matrix = getRandomPiece();
+            player.pos.y = 0;
+            player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+            if (collide(arena, player)) {
+                arena.forEach(row => row.fill(0));
+                score = 0;
+                updateSpeed();
+                alert("Game Over!");
+            }
+        }
+
+        function playerRotate() {
+            const m = player.matrix;
+            for (let y = 0; y < m.length; ++y) {
+                for (let x = 0; x < y; ++x) {
+                    [m[x][y], m[y][x]] = [m[y][x], m[x][y]];
+                }
+            }
+            m.forEach(row => row.reverse());
+            if (collide(arena, player)) {
+                m.forEach(row => row.reverse());
+                for (let y = 0; y < m.length; ++y) {
+                    for (let x = 0; x < y; ++x) {
+                        [m[x][y], m[y][x]] = [m[y][x], m[x][y]];
+                    }
+                }
+            }
+        }
+
+        let score = 0;
+        function arenaSweep() {
+            let rowCount = 1;
+            outer: for (let y = arena.length - 1; y >= 0; --y) {
+                for (let x = 0; x < arena[y].length; ++x) {
+                    if (arena[y][x] === 0) {
+                        continue outer;
+                    }
+                }
+
+                const row = arena.splice(y, 1)[0].fill(0);
+                arena.unshift(row);
+                ++y;
+
+                score += rowCount * 10;
+                rowCount *= 2;
+                
+                updateSpeed();
+            }
+        }
+
+        function updateSpeed() {
+            dropInterval = Math.max(100, 1000 - Math.floor(score / 50) * 50);
+        }
+
+        function draw() {
+            context.fillStyle = '#000';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            drawMatrix(arena, { x: 0, y: 0 });
+            drawMatrix(player.matrix, player.pos);
+
+            document.getElementById('score').innerText = 'Score: ' + score;
+        }
+
+        let dropCounter = 0;
+        let dropInterval = 1000;
+        let lastTime = 0;
+
+        function update(time = 0) {
+            const deltaTime = time - lastTime;
+            lastTime = time;
+            dropCounter += deltaTime;
+            if (dropCounter > dropInterval) {
+                playerDrop();
+            }
+            draw();
+            requestAnimationFrame(update);
+        }
+
+        const arena = createMatrix(12, 20);
+        const player = {
+            pos: { x: 0, y: 0 },
+            matrix: null
+        };
+
+        document.addEventListener('keydown', event => {
+            if (event.key === 'ArrowLeft') {
+                playerMove(-1);
+            } else if (event.key === 'ArrowRight') {
+                playerMove(1);
+            } else if (event.key === 'ArrowDown') {
+                playerDrop();
+            } else if (event.key === 'ArrowUp') {
+                playerRotate();
+            }
         });
-    }
 
-    function undraw() {
-        current.forEach(index => {
-            squares[currentPosition + index].classList.remove("tetromino");
-        });
-    }
-
-    function moveDown() {
-        undraw();
-        currentPosition += width;
-        draw();
-        addScore(1);
-    }
-
-    function addScore(points) {
-        score += points;
-        scoreDisplay.textContent = score;
-        fetch("http://localhost:3000/score", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ score })
-        }).catch(() => {});
-    }
-
-    draw();
-    setInterval(moveDown, 1000);
+        playerReset();
+        update();
